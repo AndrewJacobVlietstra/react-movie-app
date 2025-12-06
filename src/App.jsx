8b375e2560f99a8a55bee4860c778896dacb40f8
Andrew Vlietstra <vlietstra123@hotmail.com>
2025-12-06 15:01:46 -0500

    Added assets and worked on home page with search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
 import { useState } from "react";
-import "./App.css";
-
-const Card = ({ title }) => {
-	const [hasLiked, setHasLiked] = useState(false);
-
-	return (
-		<div className="card">
-			<h2>{title}</h2>
-
-			<button onClick={() => setHasLiked(true)}>Like</button>
-		</div>
-	);
-};
+import Search from "./components/Search";
 
 const App = () => {
+	const [searchTerm, setSearchTerm] = useState("");
+
 	return (
-		<div className="card-container">
-			<Card title="Star Wars" rating={5} isCool={true} />
-			<Card title="Avatar" />
-			<Card title="The Lion King" />
-		</div>
+		<main>
+			<div className="pattern" />
+			<div className="wrapper">
+				<header>
+					<img src="./hero.png" />
+					<h1>
+						Find <span className="text-gradient">Movies</span> You'll Enjoy
+						Without the Hassle
+					</h1>
+				</header>
+
+				<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+			</div>
+		</main>
 	);
 };
-
 export default App;
---
+++ src/components/Search.jsx
@@ -0,0 +1,17 @@
+const Search = ({ searchTerm, setSearchTerm }) => {
+	return (
+		<div className="search">
+			<div>
+				<img src="search.svg" />
+
+				<input
+					type="text"
+					placeholder="Search through thousands of movies"
+					onChange={(e) => setSearchTerm(e.target.value)}
+					value={searchTerm}
+				/>
+			</div>
+		</div>
+	);
+};
+export default Search;
